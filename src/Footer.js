6a0764db809f98f2a41ff02c885428995c9f3f89
Singuila01 <mbayechris201@gmail.com>
2025-05-29 22:22:10 +0200

    Modif structure pied de page

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './styles.css'
+import Contact from './pages/Contact'
 
 function Footer() {
     return (
-        <footer>
-            <div>
-                <p>© {new Date().getFullYear()} Singuila - Développeur Web</p>
-                <p>
-                    <a href="https://github.com/Singuila" target="_blank" rel="noopener noreferrer" style={{color: '#61dafb', textDecoration: 'none', margin: '0 0.5rem'}}>GitHub</a>
-                    |
-                    <a href="https://www.linkedin.com/in/singuila" target="_blank" rel="noopener noreferrer" style={{color: '#61dafb', textDecoration: 'none', margin: '0 0.5rem'}}>LinkedIn</a>
-                    |
-                    <a href="mailto:singuila@email.com" style={{color: '#61dafb', textDecoration: 'none', margin: '0 0.5rem'}}>Contact</a>
-                </p>
-            </div>
-        </footer>
+        <div>
+            {/* <Contact /> */}
+            <footer>
+                <div>
+                    <p>© {new Date().getFullYear()} Singuila - Développeur Web</p>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/Singuila01" target="_blank" rel="noopener noreferrer" className='github'></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/singuila-m-ab52b6250/" target="_blank" rel="noopener noreferrer" className='linkedin'></a>
+                        </li>
+                        <li>
+                            <a href="mailto:singuila@email.com" className='mail'></a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
     );
 }
 
