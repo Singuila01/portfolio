b9dd370cb5c7d61b0d7aaa53d73dd564b33c0394
Singuila01 <mbayechris201@gmail.com>
2025-07-07 20:24:03 +0200

    Modifications légères de la structure du site

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,49 +1,62 @@
 import React from 'react';
 import Footer from '../Footer'
-import '../styles.css'
+// import '../styles.css'
+import '../styles_new.css'
 import singuila from '../assets/singuila.jpg'
 
 const Accueil = () => (
     <div className='section'>
         <div className='content-header'>
             <header className='desktop'>
-                <h1>Bienvenue sur mon portfolio</h1>
-                <p>Je m'appelle Singuila Mbaye Amadou, développeur web passionné.</p>
-                {/* <a href='' className='button'>Voir plus</a> */}
+                <div className='hello'>
+                    <div>
+                        <p>Bienvenue,</p>
+                        <div>
+                            <h1>Je suis</h1>
+                            <h1>Singuila Mbaye Amadou</h1>
+                        </div>
+                        <p>Je suis développeur web.</p>
+                    </div>
+                    <div>
+                        <img src={singuila} />
+                    </div>
+                </div>
             </header>
             <header className='phone'>
                 <h1><span>Portfolio de</span> <br />Singuila MBAYE AMADOU</h1>
                 <p><span>Intégrateur CSS</span> en alternance</p>
             </header>
         </div>
+
         <div className='main'>
             <div id='profile' className='profile image before'>
                 <div className='qui-suis-je'>
-                    <h2>Qui suis-je ?</h2>
-                    <div>
+                    <div className='presentation'>
+                        <img src={singuila} />
                         <div>
-                            <img src={singuila} />
-                        </div>
-                        <div className='presentation'>
-                            <p>
-                                Je m'appelle <span>Singuila MBAYE AMADOU</span>.<br/>Âgé de 20 ans, je suis en <span>3e année</span> d'un <span>Bachelor Développeur Web</span> à <span>MyDigitalSchool Rennes</span>.
-                            </p>
                             <ul>
-                                <li>
-                                    <p>Nom: Singuila MBAYE AMADOU</p>
+                                <li className='prenom'>
+                                    <h2 className='name'>Singuila MBAYE AMADOU</h2>
                                 </li>
-                                <li>
-                                    <p>Age: 20 ans</p>
+                                <li className='age'>
+                                    <p>Age</p>
+                                    <p>20 ans</p>
                                 </li>
-                                <li>
-                                    <p>Lieu de résidence: Gévezé</p>
+                                <li className='residence'>
+                                    <p>Résidence</p>
+                                    <p>Gévezé, Bretagne</p>
                                 </li>
-                                <li>
-                                    <p>Etude: Bachelor Développeur Web</p>
+                                <li className='etude'>
+                                    <p>Etude</p>
+                                    <p>Bachelor Développeur Web</p>
+                                </li>
+                                <li className='travail'>
+                                    <p>Job</p>
+                                    <p>Intégrateur CSS</p>
                                 </li>
                             </ul>
                         </div>
-                    </div>                    
+                    </div>                   
                 </div>
             </div>
             <div id="skills" className='skills color before'>
@@ -74,24 +87,36 @@ const Accueil = () => (
             <div id="experiences" className='experience color before'>
                 <h2>Expériences</h2>
                 <ul>
-                    <li>
-                        <p><span>[STAGE] Technicien Junior</span></p>
-                        <p>chez <span>Biocoop</span></p>
+                    <li className='biocoop'>
+                        <div>
+                            <p>[STAGE]</p>
+                            <p>Technicien Junior</p>
+                        </div>
+                        <p>Chez <span>Biocoop</span></p>
                         <p>(Janvier - Avril 2022)</p>
                     </li>
                     <li>
-                        <p><span>[STAGE] Développeur Web</span></p>
-                        <p>chez <span>Anapoly</span></p>
+                        <div>
+                            <p>[STAGE]</p>
+                            <p>Développeur Web</p>
+                        </div>
+                        <p>Chez <span>Anapoly</span></p>
                         <p>(Mai - Juin 2023)</p>
                     </li>
                     <li>
-                        <p><span>[STAGE] Développeur Web</span></p>
-                        <p>chez <span>Myrtilles - Les mots libres</span></p>
+                        <div>
+                            <p>[STAGE]</p>
+                            <p>Développeur Web</p>
+                        </div>
+                        <p>Chez <span>Myrtilles - Les mots libres</span></p>
                         <p>(Janvier - Février 2024)</p>
                     </li>
                     <li>
-                        <p><span>[ALTERNANCE] Intégrateur CSS</span></p>
-                        <p>chez <span>Touzazimut</span></p>
+                        <div>
+                            <p>[ALTERNANCE]</p>
+                            <p>Intégrateur CSS</p>
+                        </div>
+                        <p>Chez <span>Touzazimut</span></p>
                         <p>(Septembre 2024 - Août 2025)</p>
                     </li>
                 </ul>
@@ -101,21 +126,25 @@ const Accueil = () => (
                 <h2>Projets</h2>
                 <ul>
                     <li>
-                        <p><span>CyberSafeDLS</span></p>
-                        <p>Site internet permettant de faire la prévention sur la cybersécurité.</p>
+                        <div>
+                            <p><span>[CYBERSAFEDLS]</span></p>
+                        </div>
+                        <p>Site internet permettant de faire de la prévention sur la cybersécurité et ses enjeux.</p>
                     </li>
                     <li>
-                        <p><span>TraceGPS</span></p>
+                        <div>
+                            <p><span>[TRACEGPS]</span></p>
+                        </div>
                         <p>TraceGPS est une application web permettant de tracer et partager les courses réalisées par des coureurs.</p>
                     </li>
                     <li>
-                        <p><span>LOCO</span></p>
+                        <div>
+                            <p><span>[LOCO]</span></p>
+                        </div>
                         <p>LOCO est une application mobile permettant aux particuliers de commander des produits des commerçants locaux, proche des chez eux.</p>
                     </li>
                 </ul>
             </div>
-
-            
 
             <Footer />
         </div>
